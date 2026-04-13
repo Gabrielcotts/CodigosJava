3c30c9659b10e4fcca7eafda090da1b1bc0b62a6
Gabrielcotts <gabrielcotts1444@gmail.com>
2026-04-13 08:47:25 -0300

    Add files via upload

---
+++ src/DevDojo/JavaCore/BintroduçãoDeMetodos/test/CalculadoraTest05.java
@@ -0,0 +1,17 @@
+//Código para maior entendimento de POO
+
+package MaratonaJava.test;
+import MaratonaJava.dominio.Calculadora;
+
+public class CalculadoraTest05 {
+    public static void main(String[] args) {
+
+        Calculadora calculadora = new Calculadora();
+        int a = 1;
+        int b = 2;
+        calculadora.alteraDoisNumeros(a, b);
+        System.out.println("\nCalculadoratest05");
+        System.out.println(a);
+        System.out.println(b);
+    }
+}

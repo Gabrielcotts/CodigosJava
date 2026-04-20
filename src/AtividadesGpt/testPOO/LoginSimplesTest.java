d2618a66188578488656ed835dc5c891a84fbc73
Gabrielcotts <gabrielcotts1444@gmail.com>
2026-04-20 13:57:27 -0300

    Add files via upload

---
+++ src/AtividadesGpt/testPOO/LoginSimplesTest.java
@@ -0,0 +1,26 @@
+package Atividades.testePOO;
+
+import Atividades.dominioPOO.LoginSimples;
+
+import java.util.Scanner;
+
+public class LoginSimplesTest {
+    public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite seu usuário: ");
+        String usuario = scanner.nextLine();
+
+        System.out.println("Digite sua senha: ");
+        String senha = scanner.nextLine();
+
+        if (!usuario.isEmpty() && !senha.isEmpty()) {
+            System.out.println("Erro: dados inválidos");
+        } else {
+            System.out.println("Usuário: " + usuario);
+            System.out.println("Senha: " + senha);
+        }
+    }
+}

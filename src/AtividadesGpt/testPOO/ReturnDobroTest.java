dc31f68d2a1051e3d1a1b7ceddd7324ba126e68e
Gabrielcotts <gabrielcotts1444@gmail.com>
2026-04-23 08:54:24 -0300

    Update ReturnDobroTest.java

--- src/AtividadesGpt/testPOO/ReturnDobroTest.java
+++ src/AtividadesGpt/testPOO/ReturnDobroTest.java
@@ -10,8 +10,7 @@ public class ReturnDobroTest {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Digite um número: ");
         int numero = scanner.nextInt();
-
-        ReturnDobro.dobro(numero);
-        System.out.println("O dobro do seu número é: " + numero);
+        
+        System.out.println("O dobro do seu número é: " + ReturnDobro.dobro(numero););
     }
 }

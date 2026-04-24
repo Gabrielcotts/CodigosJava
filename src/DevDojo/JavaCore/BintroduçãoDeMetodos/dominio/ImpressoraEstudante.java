df0aed3c194afd3aa3108be56e9a38bb78dd8ff0
Gabrielcotts <gabrielcotts1444@gmail.com>
2026-04-24 13:08:44 -0300

    Add imprime method to ImpressoraEstudante class
    Implement print method to display student details.

--- src/DevDojo/JavaCore/BintroduçãoDeMetodos/dominio/ImpressoraEstudante.java
+++ src/DevDojo/JavaCore/BintroduçãoDeMetodos/dominio/ImpressoraEstudante.java
@@ -1,7 +1,13 @@
+//Esse código mostra na prática os parâmetros de tipos de referências
 package MaratonaJava.dominio;
 
 public class ImpressoraEstudante {
     public void imprime(Estudante estudante){
+        System.out.println("----------------------");
+
+        estudante.nome = "Nathaly";
+        estudante.idade = 19;
+        estudante.sexo = 'F';
 
     }
 }

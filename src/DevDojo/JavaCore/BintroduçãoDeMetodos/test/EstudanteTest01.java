8c4d83a01b8d907676c3f5e2ff5f7514d435c9c9
Gabrielcotts <gabrielcotts1444@gmail.com>
2026-04-24 13:08:09 -0300

    Refactor EstudanteTest01 to use ImpressoraEstudante
    Added ImpressoraEstudante to print student details.

--- src/DevDojo/JavaCore/BintroduçãoDeMetodos/test/EstudanteTest01.java
+++ src/DevDojo/JavaCore/BintroduçãoDeMetodos/test/EstudanteTest01.java
@@ -1,12 +1,14 @@
+//Esse código mostra na prática os parâmetros de tipos de referências
 package MaratonaJava.test;
 
 import MaratonaJava.dominio.Estudante;
+import MaratonaJava.dominio.ImpressoraEstudante;
 
 public class EstudanteTest01 {
     static void main(String[] args) {
         Estudante estudante01 = new Estudante();
         Estudante estudante02 = new Estudante();
-
+        ImpressoraEstudante impressoraEstudante = new ImpressoraEstudante();
         estudante01.nome = "Gabriel";
         estudante01.idade = 21;
         estudante01.sexo = 'M';
@@ -18,6 +20,10 @@ public class EstudanteTest01 {
         estudante02.nome = "Nathaly";
         estudante02.idade = 19;
         estudante02.sexo = 'F';
+
+        impressoraEstudante.imprime(estudante01);
+
+        impressoraEstudante.imprime(estudante02);
 
         System.out.println(estudante02.nome + "\n" + estudante02.idade + "\n" + estudante02.sexo);
     }

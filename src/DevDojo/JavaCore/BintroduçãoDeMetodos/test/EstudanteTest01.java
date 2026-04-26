bc9f3080370245607bb61dd32b969c1398b73f64
Gabrielcotts <gabrielcotts1444@gmail.com>
2026-04-26 18:05:21 -0300

    Update EstudanteTest01.java

--- src/DevDojo/JavaCore/BintroduçãoDeMetodos/test/EstudanteTest01.java
+++ src/DevDojo/JavaCore/BintroduçãoDeMetodos/test/EstudanteTest01.java
@@ -1,19 +1,19 @@
 //Esse código mostra na prática os parâmetros de tipos de referências
-package MaratonaJava.test;
+package DevDojo.JavaCore.BintroduçãoDeMetodos.test;
 
-import MaratonaJava.dominio.Estudante;
-import MaratonaJava.dominio.ImpressoraEstudante;
+import DevDojo.JavaCore.BintroduçãoDeMetodos.dominio.Estudante;
+import DevDojo.JavaCore.BintroduçãoDeMetodos.dominio.ImpressoraEstudante;
 
 public class EstudanteTest01 {
     static void main(String[] args) {
         Estudante estudante01 = new Estudante();
         Estudante estudante02 = new Estudante();
         ImpressoraEstudante impressoraEstudante = new ImpressoraEstudante();
+
         estudante01.nome = "Gabriel";
         estudante01.idade = 21;
         estudante01.sexo = 'M';
 
-        System.out.println(estudante01.nome + "\n" + estudante01.idade + "\n" + estudante01.sexo);
 
         System.out.println("----------------------");
 
@@ -21,10 +21,16 @@ public class EstudanteTest01 {
         estudante02.idade = 19;
         estudante02.sexo = 'F';
 
+
         impressoraEstudante.imprime(estudante01);
 
         impressoraEstudante.imprime(estudante02);
 
-        System.out.println(estudante02.nome + "\n" + estudante02.idade + "\n" + estudante02.sexo);
+
+        System.out.println("#############");
+
+        impressoraEstudante.imprime(estudante01);
+
+        impressoraEstudante.imprime(estudante02);
     }
 }

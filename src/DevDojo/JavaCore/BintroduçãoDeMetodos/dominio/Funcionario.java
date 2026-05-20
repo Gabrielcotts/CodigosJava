7278379f048d68e79a09fd6d32abacc3376bd9d4
Gabrielcotts <gabrielcotts1444@gmail.com>
2026-05-20 13:43:52 -0300

    Update Funcionario.java

--- src/DevDojo/JavaCore/BintroduçãoDeMetodos/dominio/Funcionario.java
+++ src/DevDojo/JavaCore/BintroduçãoDeMetodos/dominio/Funcionario.java
@@ -1,1 +1,34 @@
 
+package MaratonaJava.dominio;
+
+public class Funcionario {
+
+    public String nome;
+    public int idade;
+    public double[] salarios;
+
+    public void imprimeDados() {
+        System.out.println(this.nome);
+        System.out.println(this.idade);
+        if (salarios == null){
+            return;
+        }
+        for (double salarios : salarios) {
+            System.out.print(salarios + " ");
+        }
+        imprimeMediaSalarial();
+    }
+
+    public void imprimeMediaSalarial() {
+        if (salarios == null){
+            return;
+        }
+        double media = 0;
+
+        for (double salarios : salarios){
+            media += salarios;
+        }
+        media /= salarios.length;
+        System.out.println("\nMédia salarial: " + media);
+    }
+}

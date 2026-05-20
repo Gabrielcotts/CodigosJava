9e26bf89d81530b8cc99fc3a5805a7ee18cd4f4f
Gabrielcotts <gabrielcotts1444@gmail.com>
2026-05-20 13:44:09 -0300

    Update FuncionarioTest.java

--- src/DevDojo/JavaCore/BintroduçãoDeMetodos/test/FuncionarioTest.java
+++ src/DevDojo/JavaCore/BintroduçãoDeMetodos/test/FuncionarioTest.java
@@ -1,1 +1,19 @@
+package MaratonaJava.test;
 
+import MaratonaJava.dominio.Funcionario;
+
+import java.util.Scanner;
+
+public class FuncionarioTest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Funcionario funcionario = new Funcionario();
+
+        funcionario.nome = "Sanji";
+        funcionario.idade = 21;
+        funcionario.salarios = new double[]{1200.76, 987.32, 1678.21};
+
+        funcionario.imprimeDados();
+
+    }
+}

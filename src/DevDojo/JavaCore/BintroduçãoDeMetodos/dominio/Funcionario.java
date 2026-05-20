4d2b290c70c36e99db0f0631647d41ed96d48f13
Gabrielcotts <gabrielcotts1444@gmail.com>
2026-05-20 13:57:45 -0300

    Refactor Funcionario class to use encapsulation

--- src/DevDojo/JavaCore/BintroduçãoDeMetodos/dominio/Funcionario.java
+++ src/DevDojo/JavaCore/BintroduçãoDeMetodos/dominio/Funcionario.java
@@ -1,11 +1,11 @@
-
 package MaratonaJava.dominio;
 
 public class Funcionario {
 
-    public String nome;
-    public int idade;
-    public double[] salarios;
+    private String nome;
+    private int idade;
+    private double[] salarios;
+    private   double media;
 
     public void imprimeDados() {
         System.out.println(this.nome);
@@ -23,12 +23,40 @@ public class Funcionario {
         if (salarios == null){
             return;
         }
-        double media = 0;
+
 
         for (double salarios : salarios){
             media += salarios;
         }
         media /= salarios.length;
         System.out.println("\nMédia salarial: " + media);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public double[] getSalarios() {
+        return salarios;
+    }
+
+    public void setSalarios(double[] salarios) {
+        this.salarios = salarios;
+    }
+
+    public double getMedia() {
+        return media;
     }
 }

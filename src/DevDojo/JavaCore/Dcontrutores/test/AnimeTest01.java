8ced4bea32858c5ec2eb8c4a5cdcedb39f021d36
Gabrielcotts <gabrielcotts1444@gmail.com>
2026-05-25 09:51:17 -0300

    Update AnimeTest01.java

--- src/DevDojo/JavaCore/Dcontrutores/test/AnimeTest01.java
+++ src/DevDojo/JavaCore/Dcontrutores/test/AnimeTest01.java
@@ -3,8 +3,8 @@ package DevDojo.Javacore.Dcontrutores.test;
 import DevDojo.Javacore.Dcontrutores.dominio.Anime;
 
 public class AnimeTest01 {
-    public static void main(String[] args) { 
-        Anime anime = new Anime("Dragon ball", "TV",  "ação", 800);
+    public static void main(String[] args) {
+        Anime anime = new Anime("Dragon ball", "TV",  "ação", 800, "Production IG");
         Anime anime2 = new Anime();
 
         anime.imprime();

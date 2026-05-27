b495d149110f4472d313ff938683e81f7308a271
Gabrielcotts <gabrielcotts1444@gmail.com>
2026-05-27 14:00:25 -0300

    Add files via upload

---
+++ src/DevDojo/JavaCore/Eblocosinicializacao/test/AnimeTest01.java
@@ -0,0 +1,12 @@
+package DevDojo.Javacore.Eblocosinicializacao.test;
+
+import DevDojo.Javacore.Eblocosinicializacao.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+
+    Anime anime = new Anime();
+
+
+    }
+}

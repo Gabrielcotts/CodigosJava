07a25018f72c04690e523b41a038a4aa9de95355
Gabrielcotts <gabrielcotts1444@gmail.com>
2026-05-29 13:59:26 -0300

    Update EstudanteTest.java

--- src/AtividadesGpt/PraticaDevDojo/Test/EstudanteTest.java
+++ src/AtividadesGpt/PraticaDevDojo/Test/EstudanteTest.java
@@ -1,7 +1,14 @@
 package AtividadesGpt.PraticaDevDojo.Test;
 
+import AtividadesGpt.PraticaDevDojo.Dominio.Estudante;
+
 public class EstudanteTest {
     public static void main(String[] args) {
+
+        Estudante estudante = new Estudante();
+
+        estudante.getNota();
+        estudante.calcularMedia();
 
     }
 }

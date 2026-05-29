ff96cddc85245f07fe1ca6e1f2713ce3849f6f88
Gabrielcotts <gabrielcotts1444@gmail.com>
2026-05-29 13:59:12 -0300

    Update Estudante.java

--- src/AtividadesGpt/PraticaDevDojo/Dominio/Estudante.java
+++ src/AtividadesGpt/PraticaDevDojo/Dominio/Estudante.java
@@ -2,7 +2,28 @@ package AtividadesGpt.PraticaDevDojo.Dominio;
 
 public class Estudante {
     private String nome;
-    private float nota;
+    private float[] nota = new float[3];
 
+    public float[] calcularMedia(){
+        for (float nota : this.nota) {
+            System.out.println(nota);
+        }
+        return this.nota = nota;
+    }
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public float[] getNota() {
+        return nota;
+    }
+
+    public void setNota(float[] nota) {
+        this.nota = nota;
+    }
 }

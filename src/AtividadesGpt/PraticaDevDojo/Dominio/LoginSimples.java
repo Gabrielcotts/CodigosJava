4217d18220dad26e8e5710712488d12119d2f50a
Gabrielcotts <gabrielcotts1444@gmail.com>
2026-06-03 14:02:19 -0300

    Add files via upload

---
+++ src/AtividadesGpt/PraticaDevDojo/Dominio/LoginSimples.java
@@ -0,0 +1,8 @@
+package AtividadesGpt.Pratica.dominioPOO;
+
+public class LoginSimples {
+
+        public static boolean validarLogin(String usuario, String senha) {
+        return usuario != null && !usuario.isEmpty() && senha != null && senha.length() >= 4;
+    }
+}

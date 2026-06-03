fadeb7e9bc25c4d7bd66c6460596867c411cfb99
Gabrielcotts <gabrielcotts1444@gmail.com>
2026-06-03 14:03:09 -0300

    Add files via upload

---
+++ src/AtividadesGpt/PraticaDevDojo/Test/LoginTest.java
@@ -0,0 +1,28 @@
+package AtividadesGpt.PraticaDevDojo.Test;
+
+import AtividadesGpt.PraticaDevDojo.Dominio.Login;
+
+import java.util.Scanner;
+
+public class LoginTest {
+    public static void main(String[] args) {
+
+        Login login = new Login();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Login\n");
+
+        System.out.println("Digite seu usuário: ");
+        login.usuario = scanner.nextLine();
+
+        System.out.println("Digite sua senha: ");
+        login.setSenha(scanner.nextLine());
+
+        if (login.validarUsuario(login.usuario) == true || login.validarSenha(login.getSenha())){
+            System.out.println("Login não efetuado!");
+        } else {
+            System.out.println("Login efetuado com sucesso!\nSeja bem-vindo");
+        }
+
+    }
+}

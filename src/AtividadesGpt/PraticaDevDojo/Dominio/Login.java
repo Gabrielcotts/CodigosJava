2faa2788f7ec1efbee98f83e89a3bd518874e74d
Gabrielcotts <gabrielcotts1444@gmail.com>
2026-06-03 14:03:54 -0300

    Update and rename LoginSimples.java to Login.java

---
+++ src/AtividadesGpt/PraticaDevDojo/Dominio/Login.java
@@ -0,0 +1,21 @@
+package AtividadesGpt.PraticaDevDojo.Dominio;
+
+public class Login {
+    public String usuario;
+    private String senha;
+
+    public static boolean validarUsuario(String usuario){
+        return usuario != null && !usuario.isEmpty();
+    }
+    public static boolean validarSenha(String senha){
+        return senha != null && !senha.isEmpty()  && senha.length() <= 6;
+    }
+    public String getSenha(){
+        return this.senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+}
--- src/AtividadesGpt/PraticaDevDojo/Dominio/LoginSimples.java
+++
@@ -1,8 +0,0 @@
-package AtividadesGpt.Pratica.dominioPOO;
-
-public class LoginSimples {
-
-        public static boolean validarLogin(String usuario, String senha) {
-        return usuario != null && !usuario.isEmpty() && senha != null && senha.length() >= 4;
-    }
-}

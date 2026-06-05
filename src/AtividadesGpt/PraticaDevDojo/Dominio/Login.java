1f1cd313825a83685fcf699785444b165911a63e
Gabrielcotts <gabrielcotts1444@gmail.com>
2026-06-05 11:31:11 -0300

    Fix password validation length condition

--- src/AtividadesGpt/PraticaDevDojo/Dominio/Login.java
+++ src/AtividadesGpt/PraticaDevDojo/Dominio/Login.java
@@ -4,18 +4,19 @@ public class Login {
     public String usuario;
     private String senha;
 
-    public static boolean validarUsuario(String usuario){
+    public static boolean validarUsuario(String usuario) {
         return usuario != null && !usuario.isEmpty();
     }
-    public static boolean validarSenha(String senha){
-        return senha != null && !senha.isEmpty()  && senha.length() <= 6;
+
+    public static boolean validarSenha(String senha) {
+        return senha != null && !senha.isEmpty() && senha.length() >= 6;
     }
-    public String getSenha(){
+
+    public String getSenha() {
         return this.senha;
     }
 
     public void setSenha(String senha) {
         this.senha = senha;
     }
-
 }

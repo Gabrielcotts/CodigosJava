0e60731a8ca9ed92d2264dc197b4f6bafcb05806
Gabrielcotts <gabrielcotts1444@gmail.com>
2026-06-29 21:10:14 -0300

    Add files via upload

---
+++ src/DevDojo/JavaCore/Fmodificadorestatico/dominio/Carro.java
@@ -0,0 +1,44 @@
+package DevDojo.JavaCore.Fmodificadorestatico.dominio;
+
+public class Carro {
+    private String nome;
+    private double velocidadeMaxima;
+    public static double velocidadeLimite = 250;
+//    private static double velocidadeLimite = 250;
+
+    public Carro(String nome, double velocidadeMaxima ){
+        this.nome = nome;
+        this.velocidadeMaxima = velocidadeMaxima;
+    }
+
+    public void imprimeDados(){
+        System.out.println("------------");
+        System.out.println("Nome: " + this.nome);
+        System.out.println("Velocidade Maxima: " + this.velocidadeMaxima);
+        System.out.println("Velocidade Limite: " + Carro.velocidadeLimite);
+//        System.out.println("Velocidade Limite: " + this.velocidadeLimite);
+
+    }
+
+   public void setNome(String nome){
+       this.nome = nome;
+   }
+   public String getNome(){
+       return this.nome;
+   }
+
+   public void setVelocidadeMaxima(double velocidadeMaxima){
+       this.velocidadeMaxima = velocidadeMaxima;
+   }
+   public double getVelocidadeMaxima(){
+       return this.velocidadeMaxima;
+   }
+
+//   public void setVelocidadeLimite(double velocidadeLimite){
+//       this.velocidadeLimite = velocidadeLimite;
+//   }
+//   public double getVelocidadeLimite(){
+//       return this.velocidadeLimite;
+//   }
+
+}
---
+++ src/DevDojo/JavaCore/Fmodificadorestatico/test/CarroTest01.java
@@ -0,0 +1,21 @@
+package DevDojo.JavaCore.Fmodificadorestatico.test;
+
+import DevDojo.JavaCore.Fmodificadorestatico.dominio.Carro;
+
+public class CarroTest01 {
+    public static void main(String[] args) {
+        Carro c1 = new Carro("BMW", 280);
+        Carro c2 = new Carro("Mercedes", 275);
+        Carro c3 = new Carro("Audi", 290);
+
+//      c1.setVelocidadeLimite(180);
+        Carro.velocidadeLimite = 180;
+        c1.imprimeDados();
+
+        c2.imprimeDados();
+
+        c3.imprimeDados();
+
+
+    }
+}

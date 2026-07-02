38ab4f6f461068723e137e3f2145c9cbf137ae26
Gabrielcotts <gabrielcotts1444@gmail.com>
2026-07-02 08:20:49 -0300

    Refactor CarroTest01 to use setter for velocidadeLimite

--- src/DevDojo/JavaCore/Fmodificadorestatico/test/CarroTest01.java
+++ src/DevDojo/JavaCore/Fmodificadorestatico/test/CarroTest01.java
@@ -4,18 +4,19 @@ import DevDojo.JavaCore.Fmodificadorestatico.dominio.Carro;
 
 public class CarroTest01 {
     public static void main(String[] args) {
+        //Carro.velocidadeLimite = 180;
+        Carro.setVelocidadeLimite(180);
+
         Carro c1 = new Carro("BMW", 280);
         Carro c2 = new Carro("Mercedes", 275);
         Carro c3 = new Carro("Audi", 290);
 
 //      c1.setVelocidadeLimite(180);
-        Carro.velocidadeLimite = 180;
+
         c1.imprimeDados();
 
         c2.imprimeDados();
 
         c3.imprimeDados();
-
-
     }
 }

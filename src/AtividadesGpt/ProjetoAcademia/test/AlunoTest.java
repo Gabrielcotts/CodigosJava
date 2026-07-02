b9afb856b7f8977b57d289e6e3d55e82191929ea
Gabrielcotts <gabrielcotts1444@gmail.com>
2026-07-02 20:24:59 -0300

    Refactor variable name from 'academia' to 'aluno'

--- src/AtividadesGpt/ProjetoAcademia/test/AlunoTest.java
+++ src/AtividadesGpt/ProjetoAcademia/test/AlunoTest.java
@@ -4,18 +4,16 @@ import AtividadesGpt.ProjetoAcademia.dominio.Aluno;
 
 public class AlunoTest {
     public static void main(String[] args) {
-        Aluno academia = new Aluno
+        Aluno aluno = new Aluno
                 ("Gabriel   ", 21, 92.71, 1.82, "Mensal" );
 
-
-
-      if (!academia.validadorNome()){
+      if (!aluno.validadorNome()){
               System.out.println("Nome precisa ser preenchido corretamente.");
       } else {
-          academia.imprimeDados();
-          academia.calcularIMC();
-          academia.verificadorIdade();
-          academia.aplicarDesconto();
+          aluno.imprimeDados();
+          aluno.calcularIMC();
+          aluno.verificadorIdade();
+          aluno.aplicarDesconto();
       }
     }
 }

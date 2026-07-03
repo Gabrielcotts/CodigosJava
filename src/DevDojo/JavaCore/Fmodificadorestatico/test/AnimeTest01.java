facf55a65805cc7cc53717b0c1f2be82e8246667
Gabrielcotts <gabrielcotts1444@gmail.com>
2026-07-03 08:57:47 -0300

    Add files via upload

---
+++ src/DevDojo/JavaCore/Fmodificadorestatico/test/AnimeTest01.java
@@ -0,0 +1,11 @@
+package DevDojo.JavaCore.Fmodificadorestatico.test;
+
+import DevDojo.JavaCore.Fmodificadorestatico.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime1 = new Anime();
+        Anime anime2 = new Anime();
+        Anime anime3 = new Anime();
+    }
+}
